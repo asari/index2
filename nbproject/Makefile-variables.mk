afeb6504428fa98c7f491e43fbf6d9964074460a
asari@c0d1b9d3-358b-4e5d-9d3d-7f60cfe4eafd <asari@c0d1b9d3-358b-4e5d-9d3d-7f60cfe4eafd>
2010-10-26 12:53:36 +0000

    NetBeans にインポート直後の状態
    --HG--
    extra : convert_revision : svn%3Ac0d1b9d3-358b-4e5d-9d3d-7f60cfe4eafd/misc/index2/trunk%4027

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW_Qt-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW_Qt-Windows
+CND_ARTIFACT_NAME_Debug=index2
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW_Qt-Windows/index2
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW_Qt-Windows/package
+CND_PACKAGE_NAME_Debug=index2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW_Qt-Windows/package/index2.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW_Qt-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW_Qt-Windows
+CND_ARTIFACT_NAME_Release=index2
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW_Qt-Windows/index2
+CND_PACKAGE_DIR_Release=dist/Release/MinGW_Qt-Windows/package
+CND_PACKAGE_NAME_Release=index2.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW_Qt-Windows/package/index2.tar
---
+++ nbproject/qt-Release.pro
@@ -0,0 +1,23 @@
+# This file is generated automatically. Do not edit.
+# Use project properties -> Build -> Qt -> Expert -> Custom Definitions.
+TEMPLATE = app
+DESTDIR = dist/Release/MinGW_Qt-Windows
+TARGET = index2
+VERSION = 1.0.0
+CONFIG -= debug_and_release app_bundle lib_bundle
+CONFIG += release 
+QT = core gui
+SOURCES += index2.cpp main.cpp
+HEADERS += card_pkg_factory.hpp card.hpp card_pkg.hpp index2.h factory_info.hpp card_stack.hpp
+FORMS += index2.ui
+RESOURCES +=
+TRANSLATIONS +=
+OBJECTS_DIR = build/Release/MinGW_Qt-Windows
+MOC_DIR = 
+RCC_DIR = 
+UI_DIR = 
+QMAKE_CC = gcc.exe
+QMAKE_CXX = g++.exe
+DEFINES += 
+INCLUDEPATH += 
+LIBS += 
