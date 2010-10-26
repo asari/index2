5a2ff0ec4bd51ca19ddfbbf147e013a44faaea05
asari@c0d1b9d3-358b-4e5d-9d3d-7f60cfe4eafd <asari@c0d1b9d3-358b-4e5d-9d3d-7f60cfe4eafd>
2010-10-26 12:54:57 +0000

    g++ (mingw) でコンパイルが通らなかったのを修正
    --HG--
    extra : convert_revision : svn%3Ac0d1b9d3-358b-4e5d-9d3d-7f60cfe4eafd/misc/index2/trunk%4028

--- card_pkg.hpp
+++ card_pkg.hpp
@@ -10,7 +10,7 @@ class card_pkg{
 
 public:
 	card_pkg(factory_info& i){
-		_size = i.pkg_size; // ñá/ÉJÅ[Éh
+		_size = i.pkg_size; // ÔøΩÔøΩ/ÔøΩJÔøΩ[ÔøΩh
 		name = QString();
 	}
 	card_pkg(QString n, factory_info& i){
@@ -18,7 +18,7 @@ public:
 		name = n;
 	}
 	int size(){ return _size; }
-	void push_back(card& c){ _contents.push_back(c); }
+	void push_back(const card& c){ _contents.push_back(c); }
 	std::vector<card> contents(){ return _contents; }
 	QString toString()
 	{
